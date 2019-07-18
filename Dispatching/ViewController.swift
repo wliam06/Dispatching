9edf021c6f087838381a48f39a9e418c2a625769
wliam06 <will.liiam06@gmail.com>
2019-07-18 18:39:01 +0700

    initial commit

---
+++ Dispatching/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Dispatching
+//
+//  Created by William on 18/07/19.
+//  Copyright © 2019 William. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
